c787a0921809b923d42aae1289ee766a11663bb9
Luke <lartnak@gmail.com>
2016-12-07 00:00:27 -0500

    add css bundling

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,9 @@
+var webpack = require('webpack');
 var path = require('path');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
-  entry: './app/index.js',
+  entry: ['./app/index.js', './css/master.css'],
   output: {
     path: './build',
     filename: 'app.js'
@@ -11,13 +13,26 @@ module.exports = {
     extensions: ['', '.js']
   },
   module: {
-    loaders: [{
-      test: /\.js$/,
-      exclude: /node_modules/,
-      loader: 'babel',
-      query: {
-        presets: ['es2015', 'react']
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      },
+      {
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract('style-loader', 'css-loader!postcss-loader')
       }
-    }]
+    ]
+  },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({compress: {warnings: false}}),
+    new ExtractTextPlugin('style.css')
+  ],
+  postcss: function() {
+    return [require('precss'), require('autoprefixer'), require('cssnano')]
   }
 }
